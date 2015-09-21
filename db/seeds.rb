2f15ec2c15ac8ee998cd1e493ba0c10c0083e71e
Casey Ann Cumbow <cumbow8@gmail.com>
2015-09-21 11:39:14 -0500

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Cats
+counter = 1
+10.times do
+  Cat.create!(url: "http://lorempixel.com/400/200/cats/#{counter}", description: Faker::Lorem.paragraph(2))
+  counter += 1
+end
+
+# Users
+
+User.create!(username: "penelope", password: "boom111")
+User.create!(username: "pepe", password: "boom111")
+
+# Votes
+
+TYPES = ["ZOMG! Cute!", "Amazing! So Beautiful!"]
+
+counter = 1
+10.times do
+  rand(1..6).times do
+    Cat.find(counter).votes.create!(category: TYPES.sample, IP_Address: Faker::Internet.ip_v6_address)
+  end
+  counter += 1
+end
+
+
