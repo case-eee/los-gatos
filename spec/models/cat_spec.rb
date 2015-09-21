eaf3f412368aa9b2b4159832e04bf0ad03531c8c
Casey Ann Cumbow <cumbow8@gmail.com>
2015-09-21 11:39:30 -0500

    adds model testing for cat and vote

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cat) { Cat.create!(url: "http://24.media.tumblr.com/tumblr_lkeyplqBY01qf9vi2o1_500.jpg", description: "hola i'm a cat")}
+
+  it { expect(cat).to have_many(:votes)}
+  it { expect(cat).to validate_presence_of(:url)}
+  it { expect(cat).to validate_presence_of(:description)}
+
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:cat) { Cat.create!(url: "http://24.media.tumblr.com/tumblr_lkeyplqBY01qf9vi2o1_500.jpg", description: "hola i'm a cat")}
+  let(:vote) { cat.votes.create!(category: "ZOMG! Cute!", IP_Address: Faker::Internet.ip_v6_address)}
+
+  it { expect(vote).to belong_to(:cat)}
+  it { expect(vote).to validate_presence_of(:category)}
+  it { expect(vote).to validate_presence_of(:IP_Address)}
+
 end
