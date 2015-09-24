cba3296bf42f1bfb2ede4539fb146a2aa5b4eec7
Casey Ann Cumbow <cumbow8@gmail.com>
2015-09-24 09:16:23 -0500

    reworking adding different types of votes

--- app/controllers/cats/votes_controller.rb
+++ app/controllers/cats/votes_controller.rb
@@ -5,25 +5,22 @@ class Cats::VotesController < ApplicationController
 
   end
 
-
   def create
     cat = Cat.find_by(id: params[:id])
-    # p request.env["HTTP_X_FORWARDED_FOR"]
 
-    create_zomg_vote(cat) if params[:category] == "Amazing"
-    create_amazing_vote(cat) if params[:category] == "Zomg"
+    cat.create_vote("Amazing! So Beautiful!", request.remote_ip) if params[:category] == "Amazing"
+    cat.create_vote("ZOMG! Cute!", request.remote_ip) if params[:category] == "Zomg"
 
     redirect_to cats_votes_path
   end
 
+  # private
 
-  private
+  # def create_zomg_vote(cat)
+  #   cat.votes.create!(category: "ZOMG! Cute!", IP_Address: request.remote_ip)
+  # end
 
-  def create_zomg_vote(cat)
-    cat.votes.create!(category: "ZOMG! Cute!", IP_Address: request.remote_ip)
-  end
-
-  def create_amazing_vote(cat)
-    cat.votes.create!(category: "Amazing! So Beautiful!", IP_Address: request.remote_ip)
-  end
+  # def create_amazing_vote(cat)
+  #   cat.votes.create!(category: "Amazing! So Beautiful!", IP_Address: request.remote_ip)
+  # end
 end
